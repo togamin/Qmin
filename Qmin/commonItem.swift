ed6b351a9845db59f59feed766434e686af32a81
Togami Yuki <togaminnnn@gmail.com>
2018-08-13 16:30:34 +0900

    全体像の作成

--- Qmin/commonItem.swift
+++ Qmin/commonItem.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+//テストデータ
+var QList:[String] = ["出身地は??","年収は??","好きな食べ物は??","好きな人は??","好きな動物は??"]
+var Qcomment:[String] = ["a","i","u","e","o"]
+var Qcategory:[String] = ["英語","就活","英語","英語","就活"]
+
+
 //CoreDataから質問情報を格納するための構造体
 //構造体のメリット
 
